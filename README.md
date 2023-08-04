8d34148723b9509f2b2e19e56c7e92773347d2f2
ymcbzrgn <96103924+ymcbzrgn@users.noreply.github.com>
2023-08-04 14:45:00 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Test-User-App
+ This is a simple test website developed for learning purposes. The website allows users to submit their name and surname, which gets stored in a MongoDB database. Users can view the stored user data on a separate page.
---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+var ejs = require('ejs');
+//var engines = require("consolidate");
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+
+app.engine('.ejs', ejs.__express);
+app.set('views', './pages');
+app.set('view engine', 'ejs');
+
+// Connect to the MongoDB database
+mongoose.connect('mongodb://localhost:27017/myDatabase', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+/*
+// Serve the index.html page
+app.get('/', (req, res) => {
+    res.sendFile('pages/index.html', { root: __dirname });
+});
+*/
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Handle form submissions and store data in MongoDB
+app.post('/', async (req, res) => {
+    const { name, surname } = req.body;
+    const validName = name.length >= 3;
+    const validSurname = surname.length >= 3;
+    //const validCheckbox = req.body.hasOwnProperty('checked');
+
+    if (!validName || !validSurname) {
+        return res.status(400).send('Please enter valid data');
+    }
+
+    const newUser = new User({ name, surname });
+    await newUser.save();
+    res.redirect('/update');
+});
+
+// Serve the update.html page and display all data from MongoDB
+app.get('/update', async (req, res) => {
+    try {
+        const allUsers = await User.find();
+        res.render('update', { users: allUsers });
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`);
+});
